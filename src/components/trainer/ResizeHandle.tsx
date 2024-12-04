6f37d9ce0a466380bae0e7f983d1cfe935820095
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2024-12-04 15:36:46 -0500

    Create Resize Handle

---
+++ src/components/trainer/ResizeHandle.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUpRightAndDownLeftFromCenter } from '@fortawesome/free-solid-svg-icons';
+
+interface ResizeHandleProps {
+  resizeRef: React.RefObject<HTMLDivElement>;
+  handleMouseDown: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+}
+
+const ResizeHandle: React.FC<ResizeHandleProps> = ({ resizeRef, handleMouseDown }) => {
+  return (
+    <div
+      ref={resizeRef}
+      onMouseDown={handleMouseDown}
+      className="absolute bottom-[-23px] right-[-25px] w-5 h-5 cursor-se-resize"
+    >
+      <FontAwesomeIcon
+        icon={faUpRightAndDownLeftFromCenter}
+        className="transform rotate-90"
+      />
+    </div>
+  );
+};
+
+export default ResizeHandle;
