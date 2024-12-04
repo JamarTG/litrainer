8269eb0b427957d5bba535bb8d9f11f521eb8338
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2024-12-04 04:30:33 -0500

    Updated TailwindCSS Config/Forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+import forms from "@tailwindcss/forms";
+
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {
       colors: {
         darkBackground: "#343541",
-
-        primary: "#1E3A8A", 
-        secondary: "#10B981", 
+        primary: "#1E3A8A",
+        secondary: "#10B981",
         accent: "#F59E0B",
       },
-
       fontFamily: {
-        sans: ["Inter", "sans-serif"], 
+        sans: ["Inter", "sans-serif"],
       },
     },
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [forms],
 };
