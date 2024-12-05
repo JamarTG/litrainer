e89b7a1f53968b779665abb191c68048a949b095
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2024-12-05 09:44:48 -0500

    Make Icons more appropriate

--- src/components/trainer/ControlPanel.tsx
+++ src/components/trainer/ControlPanel.tsx
@@ -1,13 +1,16 @@
 import {
   faChessKing,
   faChessQueen,
-  faChessRook,
+  faBackward,
+  faForward,
   faCircleCheck,
   faCircleXmark,
   faExternalLinkAlt,
   faFlagCheckered,
+  faCircle,
   faSquareCaretLeft,
   faSquareCaretRight,
+  faChessBoard,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Models } from "../../typings";
@@ -36,13 +39,13 @@ const ControlPanel: React.FC<ControlPanelProps> = ({
       {isDataAvailable ? (
         <div className="ml-4 flex flex-col space-y-6 p-6 bg-gray-800 rounded-lg shadow-lg w-80 flex-grow">
           <div className="flex items-center mb-4 gap-2">
-            <div className="flex items-center space-x-2 w-1/3">
+            <div className="flex items-center justify-center space-x-2 w-1/3">
               <FontAwesomeIcon
-                icon={faChessKing}
-                className="text-white text-2xl mb-2"
+                icon={faChessBoard}
+                className="text-white text-xl "
               />
               <p>
-                {puzzleIndex.x + 1} / {puzzles.length}{" "}
+                {sessionStarted ? puzzleIndex.x + 1 : 0} / {puzzles.length}{" "}
               </p>
             </div>
             <div className="flex items-center space-x-2 w-1/3">
@@ -58,66 +61,56 @@ const ControlPanel: React.FC<ControlPanelProps> = ({
           </div>
           <div className="flex flex-col justify-center mb-4 space-y-4">
             <div className="flex items-center space-x-2 bg-gray-700 p-2 rounded-lg">
-              <FontAwesomeIcon
-                icon={faChessKing}
-                className="text-white text-4xl"
-              />
-              <div>
-                <p className="text-md font-semibold">
-                  {currentPuzzle?.players.white.user}
-                </p>
-                <p className="text-sm text-gray-400">
-                  {currentPuzzle?.players.white.rating}
+              <div className="flex flex-col space-y-2">
+              <div className="flex gap-2 items-center">
+                <FontAwesomeIcon
+                icon={faCircle}
+                className="text-white text-sm"
+                />
+                <p className="text-sm">
+                {currentPuzzle?.players.white.user} (
+                {currentPuzzle?.players.white.rating})
                 </p>
               </div>
-            </div>
-            <div className="flex items-center space-x-2 bg-gray-700 p-2 rounded-lg">
-              <FontAwesomeIcon
-                icon={faChessQueen}
-                className="text-black text-4xl"
-              />
-              <div>
-                <p className="text-md font-semibold">
-                  {currentPuzzle?.players.black.user}
+              <div className="flex gap-2 items-center">
+                <FontAwesomeIcon
+                icon={faCircle}
+                className="text-black text-sm"
+                />
+                <p className="text-sm">
+                {currentPuzzle?.players.black.user} (
+                {currentPuzzle?.players.black.rating})
                 </p>
-                <p className="text-sm text-gray-400">
-                  {currentPuzzle?.players.black.rating}
-                </p>
+              </div>
               </div>
             </div>
           </div>
 
           <div className="flex items-center justify-center mt-4">
             {sessionStarted ? (
-              <div className="bg-gray-700 rounded-lg flex space-x-2 p-2">
+              <div className="rounded-lg flex space-x-2 p-2">
                 <button
                   className="hover:text-gray-400 text-white font-bold py-2 px-4 rounded-lg"
                   onClick={moveToPreviousPuzzle}
                 >
-                  <FontAwesomeIcon
-                    icon={faSquareCaretLeft}
-                    className="text-4xl"
-                  />
+                  <FontAwesomeIcon icon={faBackward} className="text-4xl" />
                 </button>
                 <button
                   className="hover:text-gray-400 text-white font-bold py-2 px-4 rounded-full"
                   onClick={moveToNextPuzzle}
                 >
-                  <FontAwesomeIcon
-                    icon={faSquareCaretRight}
-                    className="text-4xl"
-                  />
+                  <FontAwesomeIcon icon={faForward} className="text-4xl" />
                 </button>
               </div>
             ) : (
-              <div className="bg-gray-700 rounded-lg flex p-2">
+              <div className="rounded-lg flex p-2">
                 <button
                   className="hover:text-gray-400 text-white font-bold py-2 px-4 rounded-full"
                   onClick={moveToNextPuzzle}
                 >
                   <FontAwesomeIcon
                     icon={faFlagCheckered}
-                    className="text-4xl"
+                    className="text-6xl"
                   />
                 </button>
               </div>
@@ -125,18 +118,19 @@ const ControlPanel: React.FC<ControlPanelProps> = ({
           </div>
 
           <div className="flex flex-col items-center mt-4">
-            <FontAwesomeIcon
-              icon={faChessRook}
-              className="text-white text-2xl mb-2"
-            />
-            <br />
             <div className="grid grid-cols-5 gap-2">
               {puzzles.map((_, index) => (
-                <div key={index} className="flex items-center">
+                <div
+                  key={index}
+                  className="flex items-center justify-center cursor-pointer hover:scale-105 hover:opacity-80 transition-transform duration-200"
+                >
                   <FontAwesomeIcon
-                    icon={index % 2 === 0 ? faCircleCheck : faCircleXmark}
-                    className={`mr-2 ${
-                      index % 2 === 0 ? "text-green-500" : "text-red-500"
+                    icon={
+                      index <= puzzleIndex.x ? faCircleCheck : faCircleXmark
+                    }
+                    fill="white"
+                    className={`${
+                      index <= puzzleIndex.x ? "text-green-500" : "text-red-500"
                     } text-2xl`}
                   />
                 </div>
