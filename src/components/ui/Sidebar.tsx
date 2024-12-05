ac1c2791fce1f04d03ccbe2c9cfe4695aec0a46f
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2024-12-05 11:27:06 -0500

    Improve Look on Sidebar

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -4,6 +4,11 @@ import {
   faUser,
   faPuzzlePiece,
   faChessKnight,
+  faBug,
+  faLightbulb,
+  faBook,
+  faQuestionCircle,
+  faBullseye,
 } from "@fortawesome/free-solid-svg-icons";
 
 const Sidebar: React.FC = () => {
@@ -12,11 +17,11 @@ const Sidebar: React.FC = () => {
     <div className="flex flex-col justify-between h-full bg-gray-800 text-gray-100">
       <div>
         <div className="flex items-center mt-6 mb-12 ml-4">
-          <div className="relative flex">
-            <div className="absolute inset-0 bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 rounded-full blur-lg opacity-75"></div>
-            <FontAwesomeIcon icon={faChessKnight} className="relative text-4xl text-white z-10" />
-            <span className="relative text-white z-10 ml-2 text-2xl hidden md:inline">LITRAINER</span>
-          </div>
+            <div className="relative flex">
+                <div className="absolute inset-0 bg-blue-500 rounded-full blur-xl opacity-25"></div>
+                <FontAwesomeIcon icon={faChessKnight} className="relative text-2xl text-white z-10" />
+                <span className="relative text-white z-10 ml-2 text-2xl hidden md:inline whitespace-nowrap">LITRAINER</span>
+            </div>
         </div>
         <nav className="w-48 mr-2">
           <div className="grid gap-4">
@@ -26,10 +31,10 @@ const Sidebar: React.FC = () => {
                   href="#"
                   className="grid grid-cols-[40px_auto] md:grid-cols-[40px_1fr] items-center p-2 hover:bg-gray-700 rounded text-lg"
                 >
-                  <div className="flex justify-center items-center">
-                    <FontAwesomeIcon icon={faUser} className="text-2xl text-green-400" />
-                  </div>
-                  <span className="hidden md:inline">Create Session</span>
+                    <div className="flex justify-center items-center">
+                    <FontAwesomeIcon icon={faBullseye} className="text-2xl text-white" />
+                    </div>
+                  <span className="hidden md:inline whitespace-nowrap text-sm">Create Session</span>
                 </a>
               </li>
               <li>
@@ -38,9 +43,53 @@ const Sidebar: React.FC = () => {
                   className="grid grid-cols-[40px_auto] md:grid-cols-[40px_1fr] items-center p-2 hover:bg-gray-700 rounded text-lg"
                 >
                   <div className="flex justify-center items-center">
-                    <FontAwesomeIcon icon={faPuzzlePiece} className="text-2xl text-blue-400" />
+                    <FontAwesomeIcon icon={faPuzzlePiece} className="text-2xl text-white" />
                   </div>
-                  <span className="hidden md:inline">Puzzle</span>
+                  <span className="hidden md:inline whitespace-nowrap text-sm">Puzzle</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="grid grid-cols-[40px_auto] md:grid-cols-[40px_1fr] items-center p-2 hover:bg-gray-700 rounded text-lg"
+                >
+                    <div className="flex justify-center items-center">
+                    <FontAwesomeIcon icon={faQuestionCircle} className="text-2xl text-white" />
+                    </div>
+                  <span className="hidden md:inline whitespace-nowrap text-sm">Tutorial</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="grid grid-cols-[40px_auto] md:grid-cols-[40px_1fr] items-center p-2 hover:bg-gray-700 rounded text-lg"
+                >
+                  <div className="flex justify-center items-center">
+                    <FontAwesomeIcon icon={faUser} className="text-2xl text-white" />
+                  </div>
+                  <span className="hidden md:inline whitespace-nowrap text-sm">Creator</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="grid grid-cols-[40px_auto] md:grid-cols-[40px_1fr] items-center p-2 hover:bg-gray-700 rounded text-lg"
+                >
+                  <div className="flex justify-center items-center">
+                    <FontAwesomeIcon icon={faBug} className="text-2xl text-white" />
+                  </div>
+                  <span className="hidden md:inline whitespace-nowrap text-sm">Report Bug</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="grid grid-cols-[40px_auto] md:grid-cols-[40px_1fr] items-center p-2 hover:bg-gray-700 rounded text-lg"
+                >
+                    <div className="flex justify-center items-center">
+                    <FontAwesomeIcon icon={faLightbulb} className="text-2xl text-white" />
+                    </div>
+                  <span className="hidden md:inline whitespace-nowrap text-sm">Request Feature</span>
                 </a>
               </li>
             </ul>
