d2c9bddc50060d96e299bf660c0d1c8d59b8f27d
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2024-12-08 13:09:53 -0500

    Missing Tailwind Theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ import forms from "@tailwindcss/forms";
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBackground: "#4A4A4A", // Changed to a slightly lighter grey
+        primary: "#1E3A8A",
+        secondary: "#10B981",
+        accent: "#F59E0B",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [forms],
 };
