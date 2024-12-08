c96c88f9b6ad8d23fc9c4eef0bebaa6c29ba0474
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2024-12-08 00:23:26 -0500

    Refactor - Multiple Changes

---
+++ src/utils/getLichessCloudEvaluation.ts
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+const getLichessCloudEvaluation = async (fen: string, token: string, retries = 3) => {
+  try {
+    const response = await axios.get(
+      `https://lichess.org/api/cloud-eval?fen=${encodeURIComponent(fen)}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+
+    if (response.data && response.data.pvs) {
+      return response.data.pvs;
+    } else {
+      throw new Error('No moves returned from Lichess API');
+    }
+  } catch (error) {
+    console.error('Error fetching analysis from Lichess:', error);
+    if (retries > 0) {
+      console.log(`Retrying... (${retries} retries left)`);
+      return getLichessCloudEvaluation(fen, token, retries - 1);
+    } else {
+      throw error;
+    }
+  }
+};
+
+export default getLichessCloudEvaluation;
