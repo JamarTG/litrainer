29ab0dd061d895ae916c7af8e15a1c3538e3bef9
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2024-12-08 16:18:17 -0500

    Create a slightly better mobile experience

--- src/components/trainer/ControlPanel.tsx
+++ src/components/trainer/ControlPanel.tsx
@@ -1,16 +1,23 @@
+import React, { useState } from "react";
 import {
-  faBackward,
-  faForward,
   faCircleCheck,
   faCircleXmark,
-  faExternalLinkAlt,
-  faFlagCheckered,
-  faCircle,
-  faChessBoard,
+  faArrowLeft,
+  faArrowRight,
+  faChessKnight,
+  faBug,
+  faEnvelope,
+  faLightbulb,
+  faChessBishop,
+  faChessRook,
+  faChessQueen,
+  faChessKing,
+  faChessPawn,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Models } from "../../typings";
 import SkeletonControlPanel from "../ui/SkeletonControlPanel";
+import { Chess, Square } from "chess.js";
 
 interface ControlPanelProps {
   puzzles: Models.Move.Info[][];
@@ -19,6 +26,8 @@ interface ControlPanelProps {
   moveToNextPuzzle: () => void;
   moveToPreviousPuzzle: () => void;
   sessionStarted: boolean;
+  className?: string;
+  game : Chess;
 }
 
 const ControlPanel: React.FC<ControlPanelProps> = ({
@@ -27,93 +36,61 @@ const ControlPanel: React.FC<ControlPanelProps> = ({
   currentPuzzle,
   moveToNextPuzzle,
   moveToPreviousPuzzle,
-  sessionStarted,
+  game,
+  className = "",
 }) => {
+  const [showHint, setShowHint] = useState(false);
+  const [showSolution, setShowSolution] = useState(false);
+
   const isDataAvailable = currentPuzzle !== null;
+
   return (
-    <div>
+    <div className={`w-full md:w-auto mt-4 md:mt-0 md:ml-4 ${className}`}>
       {isDataAvailable ? (
-        <div className="ml-4 flex flex-col space-y-6 p-6 bg-gray-800 rounded-lg shadow-lg w-80 flex-grow">
+        <div className="flex flex-col space-y-6 p-6 bg-gray-800 rounded-lg shadow-lg w-full md:w-80 flex-grow">
           <div className="flex items-center mb-4 gap-2">
             <div className="flex items-center justify-center space-x-2 w-1/3">
+              <span className="text-white text-lg">
+                Game {puzzleIndex.x + 1}
+              </span>
+            </div>
+            <span title="Have an idea?" className="hidden md:inline whitespace-nowrap text-sm text-white ml-auto flex items-center">
               <FontAwesomeIcon
-                icon={faChessBoard}
-                className="text-white text-xl "
+                icon={faLightbulb}
+                className="text-2xl text-white mr-2"
               />
-              <p>
-                {sessionStarted ? puzzleIndex.x + 1 : 0} / {puzzles.length}{" "}
-              </p>
-            </div>
-            <div className="flex items-center space-x-2 w-1/3">
-              <a
-                className="text-blue-300 flex items-center"
-                target="_blank"
-                href={`https://lichess.org/${currentPuzzle?.game_id}`}
-              >
-                <FontAwesomeIcon icon={faExternalLinkAlt} className="mr-2" />
-                {currentPuzzle?.game_id}
-              </a>
-            </div>
+           
+            </span>
+            <button
+              title="Report Bug"
+              className="hidden md:inline whitespace-nowrap text-sm text-white ml-2"
+            >
+              <FontAwesomeIcon icon={faBug} className="text-2xl text-white" />
+            </button>
+            <span title="Contact Creator" className="hidden md:inline whitespace-nowrap text-sm text-white ml-2 flex items-center">
+              <FontAwesomeIcon
+                icon={faEnvelope}
+                className="text-2xl text-white mr-2"
+              />
+              
+            </span>
           </div>
-          <div className="flex flex-col justify-center mb-4 space-y-4">
-            <div className="flex items-center space-x-2 bg-gray-700 p-2 rounded-lg">
-              <div className="flex flex-col space-y-2">
-              <div className="flex gap-2 items-center">
-                <FontAwesomeIcon
-                icon={faCircle}
-                className="text-white text-sm"
-                />
-                <p className="text-sm">
-                {currentPuzzle?.players.white.user} (
-                {currentPuzzle?.players.white.rating})
-                </p>
-              </div>
-              <div className="flex gap-2 items-center">
-                <FontAwesomeIcon
-                icon={faCircle}
-                className="text-black text-sm"
-                />
-                <p className="text-sm">
-                {currentPuzzle?.players.black.user} (
-                {currentPuzzle?.players.black.rating})
-                </p>
-              </div>
-              </div>
-            </div>
+          <div className="flex flex-row space-x-4">
+            <button
+              onClick={moveToPreviousPuzzle}
+              className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 transition duration-200 flex items-center"
+            >
+              <FontAwesomeIcon icon={faArrowLeft} />
+            </button>
+            <button
+              onClick={moveToNextPuzzle}
+              className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 transition duration-200 flex items-center"
+            >
+              <FontAwesomeIcon icon={faArrowRight} />
+            </button>
           </div>
-
-          <div className="flex items-center justify-center mt-4">
-            {sessionStarted ? (
-              <div className="rounded-lg flex space-x-2 p-2">
-                <button
-                  className="hover:text-gray-400 text-white font-bold py-2 px-4 rounded-lg"
-                  onClick={moveToPreviousPuzzle}
-                >
-                  <FontAwesomeIcon icon={faBackward} className="text-4xl" />
-                </button>
-                <button
-                  className="hover:text-gray-400 text-white font-bold py-2 px-4 rounded-full"
-                  onClick={moveToNextPuzzle}
-                >
-                  <FontAwesomeIcon icon={faForward} className="text-4xl" />
-                </button>
-              </div>
-            ) : (
-              <div className="rounded-lg flex p-2">
-                <button
-                  className="hover:text-gray-400 text-white font-bold py-2 px-4 rounded-full"
-                  onClick={moveToNextPuzzle}
-                >
-                  <FontAwesomeIcon
-                    icon={faFlagCheckered}
-                    className="text-6xl"
-                  />
-                </button>
-              </div>
-            )}
-          </div>
-
-          <div className="flex flex-col items-center mt-4">
+          <div className="flex flex-col space-y-4">
+            <div className="text-white text-lg">Puzzles Progress</div>
             <div className="grid grid-cols-5 gap-2">
               {puzzles.map((_, index) => (
                 <div
@@ -132,6 +109,56 @@ const ControlPanel: React.FC<ControlPanelProps> = ({
                 </div>
               ))}
             </div>
+          </div>
+          <div className="flex flex-col space-y-4">
+            <button
+              onClick={() => setShowHint(!showHint)}
+              className="bg-yellow-500 text-white py-2 px-4 rounded hover:bg-yellow-700 transition duration-200"
+            >
+              <span
+                className={`transition-opacity duration-500 ${
+                  showHint ? "opacity-0" : "opacity-100"
+                }`}
+              >
+                Show Hint
+              </span>
+              <span
+                className={`absolute transition-opacity duration-500 ${
+                  showHint ? "opacity-100" : "opacity-0"
+                }`}
+              >
+                
+              {currentPuzzle && currentPuzzle.evaluation.best && (
+                <>
+                {game.get(currentPuzzle.evaluation.best.slice(0, 2) as Square).type === 'n' && <FontAwesomeIcon icon={faChessKnight} />}
+                {game.get(currentPuzzle.evaluation.best.slice(0, 2) as Square).type === 'b' && <FontAwesomeIcon icon={faChessBishop} />}
+                {game.get(currentPuzzle.evaluation.best.slice(0, 2) as Square).type === 'r' && <FontAwesomeIcon icon={faChessRook} />}
+                {game.get(currentPuzzle.evaluation.best.slice(0, 2) as Square).type === 'q' && <FontAwesomeIcon icon={faChessQueen} />}
+                {game.get(currentPuzzle.evaluation.best.slice(0, 2) as Square).type === 'k' && <FontAwesomeIcon icon={faChessKing} />}
+                {game.get(currentPuzzle.evaluation.best.slice(0, 2) as Square).type === 'p' && <FontAwesomeIcon icon={faChessPawn} />}
+                </>
+              )}
+              </span>
+            </button>
+            <button
+              onClick={() => setShowSolution(!showSolution)}
+              className="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-700 transition duration-200"
+            >
+              <span
+                className={`transition-opacity duration-500 ${
+                  showSolution ? "opacity-0" : "opacity-100"
+                }`}
+              >
+                Show Solution
+              </span>
+              <span
+                className={`absolute transition-opacity duration-500 ${
+                  showSolution ? "opacity-100" : "opacity-0"
+                }`}
+              >
+                {currentPuzzle.evaluation.best}
+              </span>
+            </button>
           </div>
         </div>
       ) : (
