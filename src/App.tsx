39d72d0706ad3ace026be3dcfeb14e6f00507f66
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2024-12-11 14:37:35 -0500

    Implement Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,39 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
 
-import About from './pages/About';
-import Home from './pages/Home';
-import Navbar from './components/ui/Navbar';
-import Help from './pages/Help';
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Navbar from "./components/ui/Navbar";
+import Help from "./pages/Help";
+import Playground from "./pages/Playground";
+
+const AppContent = () => {
+  const location = useLocation();
+  const { puzzles } = location.state || { puzzles: [] };
+
+  return (
+    <div className="flex flex-col text-violet-100">
+      <div className="w-48 h-full">
+        <Navbar />
+      </div>
+      <div className="flex-1 flex flex-col">
+        <div className="flex-1 overflow-auto">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/help" element={<Help />} />
+            <Route path="/train" element={<Playground puzzles={puzzles} />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <Router>
-      <div className="flex flex-col text-violet-100">
-        <div className="w-48 h-full">
-          <Navbar />
-        </div>
-        <div className="flex-1 flex flex-col">
-          <div className="flex-1 overflow-auto">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/help" element={<Help />} />
-            </Routes>
-          </div>
-        </div>
-      </div>
+      <AppContent />
     </Router>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,72 +1,20 @@
-import { FormEvent, useState } from "react";
-import { API_BASE_URL, INITIAL_FORM_STATE } from "../constants";
-import { Models } from "../typings";
-import { extractErrors } from "../utils/processing/extractErrors";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import TrainerForm from "../components/home/TrainerForm";
-import combineEvalAndMisplays from "../utils/processing/combineEvalAndMisplays";
-import Trainer from "./Trainer";
 
-const Home = () => {
-  const [formData, setFormData] = useState(INITIAL_FORM_STATE);
-  const [formSubmitted, setFormSubmitted] = useState<boolean>(false);
-  const [puzzles, setPuzzles] = useState<Models.Move.Info[][]>([]);
+const Home: React.FC = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    maxNoGames: 0,
+    startDate: "",
+    endDate: "",
+  });
+  const navigate = useNavigate();
 
-  const handleSubmit = async (event: FormEvent<HTMLButtonElement>) => {
+  const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
-
-    const { username, maxNoGames, startDate, endDate } = formData;
-
-    // setDate
-    const start = new Date(startDate);
-    const end = new Date(endDate);
-    end.setHours(23, 59, 59, 999);
-
-    // validateUserInput
-    if (!(username && maxNoGames > 0 && startDate && endDate)) {
-      alert("Please fill in all fields correctly");
-      return;
-    }
-
-    if (start > end) {
-      alert("Start date must be before end date.");
-      return;
-    }
-
-    setFormSubmitted(true);
-
-    // fetchGames - When no games found - we have a no data found set to true
     try {
-      const url = `${API_BASE_URL}games/user/${username}?since=${start.getTime()}&until=${end.getTime()}&max=${maxNoGames}&evals=true&analysed=true`;
-      const response = await fetch(url, {
-        headers: {
-          Accept: "application/x-ndjson",
-        },
-      });
-
-      if (!response.ok) {
-        console.error(`Error ${response.status}: ${response.statusText}`);
-        return;
-      }
-
-  
-
-      const parsedPuzzleData = await extractErrors(response);
-  
-      
-      // When no errors found - set no Errors state to true
-      if (!parsedPuzzleData) return;
-
-      const puzzles = combineEvalAndMisplays(
-        username,
-        parsedPuzzleData.misplayInfo as Models.Game.LichessGameInfo[],
-        parsedPuzzleData.moveEvaluations
-      );
-
-      
-      
-      setPuzzles(puzzles);
-
-      // If puzzles puzzles found set no errorsState to true
+      navigate("/trainer", { state: { puzzles: [] } });
     } catch (error) {
       console.error("Error fetching games:", error);
     }
@@ -74,20 +22,11 @@ const Home = () => {
 
   return (
     <div>
-      
-      {formData.username &&
-      formData.maxNoGames &&
-      formData.startDate &&
-      formData.endDate &&
-      formSubmitted ? (
-        <Trainer puzzles={puzzles} />
-      ) : (
-        <TrainerForm
-          formData={formData}
-          setFormData={setFormData}
-          handleSubmit={handleSubmit}
-        />
-      )}
+      <TrainerForm
+        formData={formData}
+        setFormData={setFormData}
+        handleSubmit={handleSubmit}
+      />
     </div>
   );
 };
