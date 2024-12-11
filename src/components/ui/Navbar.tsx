3ee9fe961066dc313f612f3aaa4603e0bc763c02
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2024-12-11 14:37:35 -0500

    Implement Routing

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -5,36 +5,49 @@ import {
   faQuestionCircle,
   faBullseye,
 } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 const Navbar: React.FC = () => {
   return (
-    <div style={{width:"96vw"}} className="sticky overflow-x-hidden flex justify-between items-center h-full text-gray-100 p-4">
+    <div
+      style={{ width: "96vw" }}
+      className="sticky overflow-x-hidden flex justify-between items-center h-full text-gray-100 p-4"
+    >
       <div className="flex items-center">
         <div className="relative flex pl-6">
-          <div className="absolute inset-0 bg-red-900 rounded-full blur-lg opacity-45"></div>
-          <FontAwesomeIcon icon={faChessKnight} className="relative text-2xl z-10" style={{ color: "#bababa" }} />
-          <span className="noto relative text-white z-10 ml-2 text-2xl whitespace-nowrap">Litrainer</span>
+          <Link to="/" className="flex items-center p-2 text-lg">
+            <FontAwesomeIcon
+              icon={faChessKnight}
+              className="relative text-2xl z-10"
+              style={{ color: "#bababa" }}
+            />
+            <span className="noto relative text-white z-10 ml-2 text-2xl whitespace-nowrap">
+              Litrainer
+            </span>
+          </Link>
         </div>
       </div>
       <nav className="flex space-x-4 ml-auto">
         <ul className="flex space-x-4">
           <li>
-            <a
-              href="#"
-              className="flex items-center p-2 text-lg"
-            >
-              <FontAwesomeIcon icon={faBullseye} className="text-2xl" style={{ color: "#bababa" }} />
+            <Link to="/train" className="flex items-center p-2 text-lg">
+              <FontAwesomeIcon
+                icon={faBullseye}
+                className="text-2xl"
+                style={{ color: "#bababa" }}
+              />
               <span className="whitespace-nowrap text-sm ml-2">TRAIN</span>
-            </a>
+            </Link>
           </li>
           <li>
-            <a
-              href="#"
-              className="flex items-center p-2 text-lg"
-            >
-              <FontAwesomeIcon icon={faQuestionCircle} className="text-2xl" style={{ color: "#bababa" }}/>
+            <Link to="/help" className="flex items-center p-2 text-lg">
+              <FontAwesomeIcon
+                icon={faQuestionCircle}
+                className="text-2xl"
+                style={{ color: "#bababa" }}
+              />
               <span className="whitespace-nowrap text-sm ml-2">HELP</span>
-            </a>
+            </Link>
           </li>
         </ul>
       </nav>
--- src/pages/Help.tsx
+++ src/pages/Help.tsx
@@ -7,32 +7,38 @@ const Help = () => {
         </h1>
 
         <div className="mb-8">
-          <div className="flex flex-col justify-center items-center md:flex-row mb-8 gap-8 rounded-md border-4 border-gray-800">
+          <div className="flex flex-col justify-center items-center md:flex-row mb-8 gap-8 rounded-md">
             <div className="md:w-1/2 p-3">
-              <div className="flex items-center justify-center h-full">
-                <i className="mb-2">
+              <div className="flex gap-10 items-center justify-center h-full">
+                <h1 className="text-9xl font-bold text-blue-500">1</h1>
+                <p className="mb-2">
                   After playing a game, click on the Analysis board button.
-                </i>
+                </p>
               </div>
             </div>
-            <div className="md:w-1/2 p-3">
+            <div className="md:w-1/2 p-3 border-4 border-gray-800">
               <img src="/computer.png" alt="analyze-png" />
             </div>
           </div>
 
-          <div className="flex flex-col justify-center items-center md:flex-row mb-8 gap-8 rounded-md border-4 border-gray-800">
-            <div className="md:w-1/2 md:order-2 p-3">
-              <i className="mb-2">
+          <div className="flex flex-col justify-center items-center md:flex-row mb-8 gap-8 rounded-md ">
+            <div className="md:w-1/2 md:order-2 p-3 flex justify-center items-center gap-10">
+              <h1 className="text-9xl font-bold text-blue-500">2</h1>
+              <p className="mb-2">
                 Then, at the bottom of the analysis page, click Request a
                 computer analysis:
-              </i>
+              </p>
             </div>
-            <div className="md:w-1/2 md:order-1 p-3">
+            <div className="md:w-1/2 md:order-1 p-3 border-4 border-gray-800">
               <img src="/analyze.png" alt="request-computer-analysis" />
             </div>
           </div>
 
-          <p> You can expect those games to be available in the time period in which you created those sessions</p>
+          <p>
+            {" "}
+            You can expect those games to be available in the time period in
+            which you created those sessions
+          </p>
         </div>
       </div>
     </div>
