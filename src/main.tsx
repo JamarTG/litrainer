cb6b950045b90ada36463b17f73862ab553d8b10
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2024-12-14 14:43:47 -0500

    Resolve SharedBuffer Error Attempt #1

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,6 @@ import App from "./App";
 import { createRoot } from "react-dom/client";
 import "./index.css"
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-    console.log('Service Worker registered with scope:', registration.scope);
-  }).catch(function(error) {
-    console.log('Service Worker registration failed:', error);
-  });
-}
-
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
