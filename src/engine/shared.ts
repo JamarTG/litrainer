bef5596f910ea46179713bd832c3da7c76f9c2df
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2024-12-14 15:18:07 -0500

    Attempt 3 SharedBuffer Error

--- src/engine/shared.ts
+++ src/engine/shared.ts
@@ -4,4 +4,5 @@ export const isWasmSupported = () =>
     Uint8Array.of(0x0, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00)
   );
 
-export const isMultiThreadSupported = () => true
+export const isMultiThreadSupported = () =>
+  typeof SharedArrayBuffer !== "undefined" && window.crossOriginIsolated;
