5c0a0844daea0d7d7bc0be647dc2ec9ea8433b53
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2024-12-19 11:11:09 -0500

    Create a Loading Screen

--- src/components/loader/index.tsx
+++ src/components/loader/index.tsx
@@ -1,25 +1,39 @@
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faChessPawn,
-  faChessKnight,
-  faChessBishop,
-  faChessRook,
-  faChessQueen,
-  faChessKing,
-} from "@fortawesome/free-solid-svg-icons";
-
 const LoadingScreen = () => (
   <div className="flex flex-col items-center justify-center h-screen">
-    <div className="flex items-center justify-center space-x-2">
-      <FontAwesomeIcon icon={faChessPawn} className="text-3xl animate-bounce" />
-      <FontAwesomeIcon icon={faChessKnight} className="text-3xl animate-bounce delay-100" />
-      <FontAwesomeIcon icon={faChessBishop} className=" text-3xl animate-bounce delay-200" />
-      <FontAwesomeIcon icon={faChessRook} className=" text-3xl animate-bounce delay-300" />
-      <FontAwesomeIcon icon={faChessQueen} className="text-3xl animate-bounce delay-400" />
-      <FontAwesomeIcon icon={faChessKing} className=" text-3xl animate-bounce delay-500" />
+    <div className="text-2xl text-violet-100 mt-4 animate-pulse">
+      <div className="flex items-center justify-center space-x-2">
+        <img
+          src="svgs/pieces/wP.svg"
+          alt="Pawn"
+          className="w-10 h-10 animate-bounce"
+        />
+        <img
+          src="svgs/pieces/wN.svg"
+          alt="Knight"
+          className="w-10 h-10 animate-bounce delay-100"
+        />
+        <img
+          src="svgs/pieces/wB.svg"
+          alt="Bishop"
+          className="w-10 h-10 animate-bounce delay-200"
+        />
+        <img
+          src="svgs/pieces/wR.svg"
+          alt="Rook"
+          className="w-10 h-10 animate-bounce delay-300"
+        />
+        <img
+          src="svgs/pieces/wQ.svg"
+          alt="Queen"
+          className="w-10 h-10 animate-bounce delay-400"
+        />
+        <img
+          src="svgs/pieces/wK.svg"
+          alt="King"
+          className="w-10 h-10 animate-bounce delay-500"
+        />
+      </div>
     </div>
-    <div className="text-2xl text-violet-100 mt-4">Loading...</div>
   </div>
 );
 
