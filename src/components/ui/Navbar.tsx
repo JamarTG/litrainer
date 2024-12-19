b30966ad702952aa43540d5c7aff04e1cc987df1
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2024-12-19 11:11:09 -0500

    Create a Loading Screen

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -15,14 +15,16 @@ const Navbar: React.FC = () => {
       <div className="flex items-center">
         <div className="relative flex pl-6">
           <Link to="/" className="flex items-center p-2 text-lg">
-            <FontAwesomeIcon
+            {/* <FontAwesomeIcon
               icon={faChessKnight}
               className="relative text-2xl z-10"
               style={{ color: "#bababa" }}
-            />
-            <span className="noto relative text-white z-10 ml-2 text-2xl whitespace-nowrap">
+            /> */}
+            <img src="/logo.svg" width={40} alt="" />
+            <span className="noto relative text-white z-10 ml-2 text-xl whitespace-nowrap">
               Litrainer
             </span>
+            
           </Link>
         </div>
       </div>
@@ -30,21 +32,13 @@ const Navbar: React.FC = () => {
         <ul className="flex space-x-4">
           <li>
             <Link to="/train" className="flex items-center p-2 text-lg">
-              <FontAwesomeIcon
-                icon={faBullseye}
-                className="text-2xl"
-                style={{ color: "#bababa" }}
-              />
+              
               <span className="whitespace-nowrap text-sm ml-2">TRAIN</span>
             </Link>
           </li>
           <li>
             <Link to="/help" className="flex items-center p-2 text-lg">
-              <FontAwesomeIcon
-                icon={faQuestionCircle}
-                className="text-2xl"
-                style={{ color: "#bababa" }}
-              />
+             
               <span className="whitespace-nowrap text-sm ml-2">HELP</span>
             </Link>
           </li>
