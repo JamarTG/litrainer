3fc4ca29fb593f966b24b9d06e902da5578bcf7a
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2024-12-19 11:43:18 -0500

    Change Logo

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -3,22 +3,14 @@ import { Link } from "react-router-dom";
 
 const Navbar: React.FC = () => {
   return (
-    <div
-      className="w-96 sticky overflow-x-hidden flex justify-between items-center h-full"
-    >
+    <div className="w-96 sticky overflow-x-hidden flex justify-between items-center h-full">
       <div className="flex items-center">
         <div className="relative flex pl-6">
-          <Link to="/" className="flex items-center p-2 text-lg">
-            {/* <FontAwesomeIcon
-              icon={faChessKnight}
-              className="relative text-2xl z-10"
-              style={{ color: "#bababa" }}
-            /> */}
-            <img src="/logo.svg" width={40} alt="" />
+          <Link to="/" className="flex items-center p-2 text-xl">
+            <img src="/logo.png" width={30} alt="" className="border-1/2"/>
             <span className="noto relative text-white z-10 ml-2 text-xl whitespace-nowrap">
-              Litrainer
-            </span>
             
+            </span>
           </Link>
         </div>
       </div>
@@ -26,13 +18,11 @@ const Navbar: React.FC = () => {
         <ul className="flex space-x-4">
           <li>
             <Link to="/train" className="flex items-center p-2 text-lg">
-              
               <span className="whitespace-nowrap text-sm ml-2">TRAIN</span>
             </Link>
           </li>
           <li>
             <Link to="/help" className="flex items-center p-2 text-lg">
-             
               <span className="whitespace-nowrap text-sm ml-2">HELP</span>
             </Link>
           </li>
