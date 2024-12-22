989c3af1e06a04386aa64c3404c0d829196271a2
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2024-12-22 10:41:58 -0500

    Refactor useChangePuzzle

--- src/hooks/useChangePuzzle.ts
+++ src/hooks/useChangePuzzle.ts
@@ -1,76 +1,48 @@
-import { useState,  Dispatch, SetStateAction } from "react";
+import { useState, Dispatch, SetStateAction } from "react";
 import { STARTINGPOSFEN } from "../constants";
-import { Game } from "../types/game";
+import { Puzzle, PuzzleIndex } from "../types/puzzle";
 
 const useChangePuzzle = (
-  puzzles: Game.Info[][],
+  puzzles: Puzzle[][],
   sessionStarted: boolean,
   setSessionStarted: Dispatch<SetStateAction<boolean>>,
-  setCurrentPuzzle : Dispatch<SetStateAction<Game.Info | null>>
+  setCurrentPuzzle: Dispatch<SetStateAction<Puzzle | null>>
 ) => {
-
-  const [puzzleIndex, setPuzzleIndex] = useState<Game.Index>({
-    x: 0,
-    y: 0,
-  });
+  const [puzzleIndex, setPuzzleIndex] = useState<PuzzleIndex>({ x: 0, y: 0 });
   const [fen, setFen] = useState<string>(STARTINGPOSFEN);
- 
 
   const moveToNextPuzzle = () => {
     if (puzzles.length === 0) return;
-
-    let newIndex: Game.Index;
-    let newFen: string;
 
     if (!sessionStarted) {
       setSessionStarted(true);
     }
 
     if (puzzleIndex.y + 1 < puzzles[puzzleIndex.x]?.length) {
-      newIndex = { x: puzzleIndex.x, y: puzzleIndex.y + 1 };
-      newFen = puzzles[puzzleIndex.x][puzzleIndex.y + 1].fenBeforeOpponentMove;
+      setCurrentPuzzle(puzzles[puzzleIndex.x][puzzleIndex.y + 1]);
+      setPuzzleIndex({ x: puzzleIndex.x, y: puzzleIndex.y + 1 });
+      setFen(puzzles[puzzleIndex.x][puzzleIndex.y + 1].fen.previous);
     } else if (puzzleIndex.x + 1 < puzzles.length) {
-      newIndex = { x: puzzleIndex.x + 1, y: 0 };
-      newFen = puzzles[puzzleIndex.x + 1][0].fenBeforeOpponentMove;
-    } else {
-      return;
+      setCurrentPuzzle(puzzles[puzzleIndex.x + 1][0]);
+      setPuzzleIndex({ x: puzzleIndex.x + 1, y: 0 });
+      setFen(puzzles[puzzleIndex.x + 1][0].fen.previous);
     }
-    setCurrentPuzzle(puzzles[newIndex.x][newIndex.y]);
-    setPuzzleIndex(newIndex);
-    setFen(newFen);
   };
 
   const moveToPreviousPuzzle = () => {
     if (puzzles.length === 0) return;
 
-    let newIndex: Game.Index;
-    let newFen: string;
-
     if (puzzleIndex.y > 0) {
-      newIndex = { x: puzzleIndex.x, y: puzzleIndex.y - 1 };
-      newFen = puzzles[puzzleIndex.x][puzzleIndex.y - 1].fenBeforeOpponentMove;
+      setCurrentPuzzle(puzzles[puzzleIndex.x][puzzleIndex.y - 1]);
+      setPuzzleIndex({ x: puzzleIndex.x, y: puzzleIndex.y - 1 });
+      setFen(puzzles[puzzleIndex.x][puzzleIndex.y - 1].fen.previous);
     } else if (puzzleIndex.x > 0) {
-      newIndex = {
-        x: puzzleIndex.x - 1,
-        y: puzzles[puzzleIndex.x - 1].length - 1,
-      };
-      newFen = puzzles[puzzleIndex.x - 1][puzzleIndex.y - 1].fenBeforeOpponentMove;
-    } else {
-      return;
+      const newY = puzzles[puzzleIndex.x - 1].length - 1;
+      setCurrentPuzzle(puzzles[puzzleIndex.x - 1][newY]);
+      setPuzzleIndex({ x: puzzleIndex.x - 1, y: newY });
+      setFen(puzzles[puzzleIndex.x - 1][newY].fen.previous);
     }
-
-    setCurrentPuzzle(puzzles[newIndex.x][newIndex.y]);
-    setPuzzleIndex(newIndex);
-    setFen(newFen);
   };
-
-  console.log({
-    puzzleIndex,
-    fen,
-    setFen,
-    moveToNextPuzzle,
-    moveToPreviousPuzzle,
-  })
 
   return {
     puzzleIndex,
