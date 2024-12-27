afc0317ef6d00ac8ac8cf62cb020cc4755a6e393
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2024-12-26 21:58:15 -0500

    include analysis source

---
+++ src/features/ControlPanel/components/AnalysisSource.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+interface AnalysisSourceProps {
+  source: "LichessApi" | "Stockfish" | "Local" | null;
+}
+
+const AnalysisSource: React.FC<AnalysisSourceProps> = ({ source }) => {
+  return (
+    source && (
+      <div className="flex">
+        Analysis Source
+        {source === "Stockfish" && (
+          <img
+            src="/images/general/png/stockfish.png"
+            width={30}
+            alt="Stockfish"
+          />
+        )}
+        {source === "LichessApi" && (
+          <img src="/images/general/png/lichess.png" width={30} alt="Lichess" />
+        )}
+        {source === "Local" && (
+          <img src="/images/general/png/logo.png" width={30} alt="Local" />
+        )}
+      </div>
+    )
+  );
+};
+
+export default AnalysisSource;
