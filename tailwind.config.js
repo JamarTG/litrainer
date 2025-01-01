a630beb2f3ae372887bb1b372756b860cc7c624e
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-01-01 14:08:13 -0500

    undo ui changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,12 @@ export default {
   theme: {
     extend: {
       colors: {
-        darkBackground: "#1A1A1A", // Changed to a slightly lighter grey
+        darkBackground: '#1a202c',
         accent: "#277F71",  // green
         primary: "#282828", // blobs
         secondary: "#2F2E2E",
         tertiary: "#424242",
-        offWhite: "#FAFAFA",
+        offWhite: "#c9c8c7",
       },
     },
   },
