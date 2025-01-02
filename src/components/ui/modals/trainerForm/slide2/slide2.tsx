cfab9ce0ca8bb33e75eedb14e966adf4e1c195cc
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-01-02 10:38:41 -0500

    remove time and specific player fields

--- src/components/ui/modals/trainerForm/slide2/slide2.tsx
+++ src/components/ui/modals/trainerForm/slide2/slide2.tsx
@@ -14,54 +14,6 @@ const SlideTwo: React.FC<SlideTwoProps> = ({ handleInputChange }) => {
     <div className="grid gap-6 px-4">
       <div className="grid gap-4">
         <Dates handleInputChange={handleInputChange} />
-
-        <div className="flex justify-between">
-          <div className="grid gap-2 w-[250px]">
-            <h1 className="text-landingText text-sm text-offWhite">Player</h1>
-            <input
-              className="flex  w-full  bg-secondary   h-[32px]   outline-none text-textwhite caret-accent   text-offWhite rounded-lg border border-shadowGray px-2.5 text-sm placeholder:text-muted "
-              placeholder="JamariTheGreat"
-              // value={formData.username}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className=" grid gap-2 w-[128px] ">
-            <h1 className="text-landingText text-sm text-offWhite">Time</h1>
-            <div className="relative  flex items-center">
-              <input
-                className="flex  w-full  bg-secondary   h-[32px]   outline-none text-textwhite caret-accent   text-offWhite rounded-lg border border-shadowGray px-2.5 text-sm placeholder:text-muted  "
-                placeholder="12:00"
-                onChange={handleInputChange}
-                ref={timeDropdown.triggerRef}
-                onClick={timeDropdown.toggleDropdown}
-              />
-              <svg
-                width="1em"
-                height="1em"
-                fill="currentColor"
-                viewBox="0 0 512 512"
-                className="absolute right-2 text-muted pointer-events-none"
-              >
-                <path
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={48}
-                  d="m136 208 120-104 120 104m-240 96 120 104 120-104"
-                />
-              </svg>
-            </div>
-            {timeDropdown.isOpen &&
-              ReactDOM.createPortal(
-                <div
-                  ref={timeDropdown.dropdownRef}
-                  className={`z-50 shadow-2xl  `}
-                ></div>,
-                document.body
-              )}
-          </div>
-        </div>
       </div>
     </div>
   );
