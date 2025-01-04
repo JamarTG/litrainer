8d56e1f81e725186eae89f2d5f3b17fecdc3d9ff
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-01-03 20:21:22 -0500

    update home text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -107,7 +107,7 @@ const Home: React.FC = () => {
       <div className="w-1/2 text-white text-xl p-4 text-center">
         This website is still under construction. Some functionalities have
         not been implemented. Click the button and use the default parameters on the modal to
-        access and try the Playground.
+        access and try the Playground. Use your Lichess username if you have one
       </div>
       <SubmitButtonWithModal
         text="Try the Trainer ❤️"
