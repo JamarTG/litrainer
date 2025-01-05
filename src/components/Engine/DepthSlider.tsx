a3241f02f80251f05062c6201e23dd7171ea7e3e
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-01-05 15:11:28 -0500

    Temporary DepthSlider Component

---
+++ src/components/Engine/DepthSlider.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+const DepthSlider = () => {
+    const [depth, setDepth] = useState(0);
+  return (
+    <div className="flex">
+      
+      <input
+        onChange={(e) => setDepth(parseInt(e.target.value))}
+        id="minmax-range"
+        type="range"
+        min="0"
+        max="20"
+        value={depth}
+        className="w-64 h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer"
+      />
+    </div>
+  );
+};
+
+export default DepthSlider;
