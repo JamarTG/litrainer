f1cfcb3fbfe4a41c6fcc8934bf32b3a8cf088165
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-01-08 18:50:21 -0500

    Implement Theme Changer

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { BrowserRouter } from "react-router-dom";
 import { EngineName } from "./types/engine";
 import Router from "./routes/router";
-import "./App.css"
+import "./App.css";
 import { EngineProvider } from "./context/EngineContext";
 import { PuzzleProvider } from "./context/PuzzleContext";
+import { ThemeProvider } from "./context/ThemeContext";
 
 const App = () => {
   return (
     <BrowserRouter>
       <EngineProvider initialEngineName={EngineName.Stockfish16_1Lite}>
         <PuzzleProvider>
-          <Router />
+          <ThemeProvider>
+            <Router />
+          </ThemeProvider>
         </PuzzleProvider>
       </EngineProvider>
     </BrowserRouter>
--- src/components/Form/SubmitButtomWithModal.tsx
+++ src/components/Form/SubmitButtomWithModal.tsx
@@ -29,7 +29,8 @@ const SubmitButtonWithModal = () => {
       <button
         onClick={handleToggleModal}
       >
-        <span className="icon text-2xl">&#xe012;</span>
+        <span className="icon text-2xl">&#xe02d;</span>
+        
       </button>
     </div>
   );
