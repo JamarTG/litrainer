c0fc980fb61350a32663b9d33eadf9ba111554c3
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-01-08 21:34:17 -0500

    Implement Scrollable History

--- src/components/Puzzle/History.tsx
+++ src/components/Puzzle/History.tsx
@@ -4,21 +4,21 @@ interface HistoryProps {
   jumpToPuzzle: (index: number) => void;
 }
 
-const History: React.FC<HistoryProps> = ({ history, jumpToPuzzle}) => {
+const History: React.FC<HistoryProps> = ({ history, jumpToPuzzle }) => {
   return (
     <div>
-      <div className="grid grid-cols-10 gap-2 m-4">
+      <div className="grid grid-cols-10 grid-rows-5 m-4 h-48 overflow-y-scroll overflow-x-hidden justify-start items-start" ref={el => { if (el) el.scrollTop = el.scrollHeight; }}>
         {Object.entries(history).map(([key, value]) => (
           <button
-        key={key}
-        onClick={() => jumpToPuzzle(parseInt(key))}
-        className="p-1 rounded transition-transform transform hover:scale-105 cursor-pointer active:scale-95"
+            key={key}
+            onClick={() => jumpToPuzzle(parseInt(key))}
+            className="p-1 m-1 rounded transition-transform transform hover:scale-105 cursor-pointer active:scale-95"
           >
-        <img
-          src={`/assets/evals/${value || "Blank"}.svg`}
-          alt={value}
-          width={20}
-        />
+            <img
+              src={`/assets/evals/${value || "Blank"}.svg`}
+              alt={value}
+              width={20}
+            />
           </button>
         ))}
       </div>
