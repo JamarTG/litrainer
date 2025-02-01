d906ff57c0aa3cab5ae1947675f729da8ed68236
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-02-01 18:28:31 -0500

    Fix 'Cannot find constants file error'

--- src/hooks/useBoardSize.ts
+++ src/hooks/useBoardSize.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect, RefObject } from 'react';
 import { calculateBoardSize } from '../utils/board';
-import { BOARD_DIMENSIONS } from '../constants';
+import { BOARD_DIMENSIONS } from '../constants/board';
 
 const useBoardSize = (boardRef: RefObject<HTMLDivElement>) => {
   const [boardSize, setBoardSize] = useState<number>(BOARD_DIMENSIONS.INITIAL_SIZE);
