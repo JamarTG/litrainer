299e4c3788a245bd8b958e58fffeb27f63ac0506
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-02-15 07:06:46 -0500

    Set Theme On Body

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,18 +1,23 @@
-import React, { createContext, ReactNode, useState } from "react";
+import React, { createContext, ReactNode, useState, useEffect } from "react";
 
 export interface ThemeContextType {
   theme: "light" | "dark";
   setTheme: React.Dispatch<React.SetStateAction<"light" | "dark">>;
 }
 
-export const ThemeContext = createContext<ThemeContextType>(
-  {} as ThemeContextType
-);
+export const ThemeContext = createContext<ThemeContextType>({} as ThemeContextType);
 
-export const ThemeProvider: React.FC<{ children: ReactNode }> = ({
-  children,
-}) => {
+export const ThemeProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const [theme, setTheme] = useState<"light" | "dark">("light");
+
+  useEffect(() => {
+    // Add or remove dark class on the body element
+    if (theme === "dark") {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }, [theme]);
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: 'class', // enables class-based dark mode
   theme: {
     extend: {
       colors: {
-        darkBackground: "#1A1A1A", // Changed to a slightly lighter grey
-        accent: "#287F71",  // green
-        primary: "#111111", // blobs
-        secondary: "#191919",
-        tertiary: "#2D2D2D",
-        quaternary: "#222222",
-        shadowGray: "#2a2a2a",
-        cloudGray: "#b4b4b4",
-        offWhite: "#FAFAFA",
-        shadowWhite: "#F9F9F9",
-        muted: "#606060"
-      },
-      animation: {
-        flip: "flip 6s infinite steps(2, end)",
-        rotate: "rotate 3s linear infinite both",
-      },
-      keyframes: {
-        flip: {
-          to: {
-            transform: "rotate(360deg)",
-          },
-        },
-        rotate: {
-          to: {
-            transform: "rotate(90deg)",
-          },
-        },
+        lightBg: "#ffffff",
+        lightText: "#4d4d4d",
+        darkBg: "#363434",
+        darkText: "white",
       },
     },
   },
