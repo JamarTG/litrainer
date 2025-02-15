253eb0406b92b23f4abdb415074b21a4a6d361bc
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-02-15 07:09:11 -0500

    Remove Unused Theme Wrapper and Loading Screen

--- src/components/LoadingScreen/LoadingScreen.tsx
+++
@@ -1,9 +0,0 @@
-const LoadingScreen = () => (
-  <div className="flex flex-col items-center justify-center h-screen">
-    <div className="text-2xl text-violet-100 mt-4 animate-pulse">
-      loading...
-    </div>
-  </div>
-);
-
-export default LoadingScreen;
