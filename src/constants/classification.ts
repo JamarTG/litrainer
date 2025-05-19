e148dd013da200056e8c8b68ecab1b61482324c9
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-05-19 18:35:46 -0500

    Add MoveClassificationImages to constants

--- src/constants/classification.ts
+++ src/constants/classification.ts
@@ -9,6 +9,17 @@ export enum MoveClassification {
   Brilliant = "Brilliant"
 }
 
+export const MoveClassificationImages: Record<MoveClassification, string> = {
+  [MoveClassification.Book]: "/move-quality/book.svg",
+  [MoveClassification.Best]: "/move-quality/best.svg",
+  [MoveClassification.Excellent]: "/move-quality/excellent.svg",
+  [MoveClassification.Good]: "/move-quality/good.svg",
+  [MoveClassification.Inaccuracy]: "/move-quality/inaccuracy.svg",
+  [MoveClassification.Mistake]: "/move-quality/mistake.svg",
+  [MoveClassification.Blunder]: "/move-quality/blunder.svg",
+  [MoveClassification.Brilliant]: "/move-quality/brilliant.svg",
+};
+
 export const ClassificationColors: Record<string, string> = {
   [MoveClassification.Book]: "#a88865",
   [MoveClassification.Best]: "#96bc4b",
