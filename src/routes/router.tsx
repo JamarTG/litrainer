19c85e9c8ce2991dae8d3f82f36c174d7f61a638
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-05-19 12:10:45 -0500

    Remove unused Routes

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,8 +1,5 @@
 import { Route, Routes, useLocation } from "react-router-dom";
-import About from "../pages/About";
-import Help from "../pages/Help";
 import Playground from "../pages/Playground";
-import NotFound from "../pages/NotFound";
 import Paths from "./paths";
 
 export default function Router() {
@@ -13,9 +10,6 @@ export default function Router() {
     <>
       <Routes>
         <Route path={Paths.HOME} element={<Playground puzzles={puzzles} />} />
-        <Route path={Paths.ABOUT} element={<About />} />
-        <Route path={Paths.HELP} element={<Help />} />
-        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
