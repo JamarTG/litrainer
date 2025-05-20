bbb83ecadca53843a907df7b3c4a86af1dac3a14
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-05-20 17:27:12 -0500

    Resolve linting errors in storage utils

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,12 +1,12 @@
 export const saveToLocalStorage = (key: string, value: unknown) => {
-  try {
-    localStorage.setItem(key, JSON.stringify(value));
-  } catch {}
+  localStorage.setItem(key, JSON.stringify(value));
 };
 
 export const getFromLocalStorage = (key: string) => {
   try {
     const value = localStorage.getItem(key);
     return value ? JSON.parse(value) : null;
-  } catch {}
+  } catch {
+    console.error("Failed to fetch from localStorage")
+  }
 };
