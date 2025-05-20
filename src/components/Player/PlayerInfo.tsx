820998f6027f10e377eec45dba19bde3105f003e
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-05-19 20:04:06 -0500

    Rename PlayerBadge

--- src/components/Player/PlayerInfo.tsx
+++ src/components/Player/PlayerInfo.tsx
@@ -1,4 +1,8 @@
-import { PlayerIcon, PatronIcon, PlayerTitle, PlayerName,PlayerRating} from ".";
+import PatronIcon from "./PatronIcon";
+import PlayerIcon from "./PlayerIcon";
+import PlayerName from "./PlayerName";
+import PlayerRating from "./PlayerRating";
+import PlayerTitle from "./PlayerTitle";
 
 interface PlayerInfoProps {
   color: "w" | "b";
@@ -7,13 +11,12 @@ interface PlayerInfoProps {
 const PlayerInfo: React.FC<PlayerInfoProps> = ({ color }) => {
   return (
     <div className="noto player-color flex justify-center items-center text-xs lg:text-base">
-  <PlayerIcon color={color} />
-  <PatronIcon color={color} />
-  <PlayerTitle color={color} />
-  <PlayerName color={color} />
-  <PlayerRating color={color} />
-</div>
-
+      <PlayerIcon color={color} />
+      <PatronIcon color={color} />
+      <PlayerTitle color={color} />
+      <PlayerName color={color} />
+      <PlayerRating color={color} />
+    </div>
   );
 };
 
--- src/constants/sound.ts
+++
@@ -1,9 +0,0 @@
-export const SOUND_ACTIONS = {
-  MOVE: "move",
-  CAPTURE: "capture",
-  CHECK: "check",
-  CASTLE: "castle",
-  PROMOTION: "promotion",
-  START: "start",
-  END: "end",
-};
