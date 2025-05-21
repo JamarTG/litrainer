20f10986157ca2773c9a8dd1189bb6d97a4c42e4
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-05-20 23:29:16 -0500

    Update no games found component

--- src/components/panel/NoGamesFound.tsx
+++ src/components/panel/NoGamesFound.tsx
@@ -5,13 +5,12 @@ import { NO_GAMES_FOUND_COLORS } from "../../constants/theme";
 
 const NoGamesFound = () => {
   const theme = useSelector((state: RootState) => state.theme.theme);
-  const headingColor = theme === "light" ? NO_GAMES_FOUND_COLORS.light.heading : NO_GAMES_FOUND_COLORS.dark.heading;
-  const textColor = theme === "light" ? NO_GAMES_FOUND_COLORS.light.text : NO_GAMES_FOUND_COLORS.dark.text;
+  const { heading, text } = NO_GAMES_FOUND_COLORS[theme];
 
   return (
     <div className="w-full md:w-[400px] h-[600px] flex flex-col gap-4 justify-center items-center p-6">
-      <h2 className={`text-xl font-bold text-center ${headingColor}`}>No Games Found on Specified Parameters</h2>
-      <p className={`text-base text-center ${textColor}`}>Try adjusting your search or explore other puzzles!</p>
+      <h2 className={`text-xl font-bold text-center ${heading}`}>No Games Found on Specified Parameters</h2>
+      <p className={`text-base text-center ${text}`}>Try adjusting your search or explore other puzzles!</p>
       <div className="mt-4">
         <SubmitButtonWithModal />
       </div>
