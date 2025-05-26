60d5e6bd9c53472b067b748fcb48ae301d00931b
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-05-26 02:45:42 -0500

    Removed a few Unnecessary files

--- src/components/playArea/helpers/squareStyles.ts
+++ src/components/playArea/helpers/squareStyles.ts
@@ -1,42 +1,9 @@
-import { Move, Square } from "chess.js";
+import { Move} from "chess.js";
 import { CSSProperties } from "react";
-import { Classification } from "../../../types/classification";
-import { ClassificationColors, MoveClassification } from "../../../constants/classification";
 import {
-  BASE_SQUARE_HIGHLIGHT_STYLES,
-  CLASSIFICATION_OPACITY,
-  DEFAULT_CLASSIFICATION_COLOR,
   LEGAL_MOVE_GRADIENTS,
 } from "../../../constants/board";
 
-export const getCustomSquareStyles = (
-  dstSquare: Square | null,
-  srcSquare: Square | null,
-  classification: Classification | null,
-  moveSquares: Record<string, CSSProperties>,
-  isLoadingEvaluation: boolean
-) => {
-  if (!dstSquare || !srcSquare) {
-    return moveSquares; 
-  }
-
-  const baseStyles = {
-    ...BASE_SQUARE_HIGHLIGHT_STYLES,
-    backgroundColor:
-      classification && !isLoadingEvaluation
-        ? ClassificationColors[MoveClassification[classification as keyof typeof MoveClassification]]
-        : DEFAULT_CLASSIFICATION_COLOR,
-  };
-
-  return {
-    ...moveSquares,
-    [dstSquare]: baseStyles,
-    [srcSquare]: {
-      ...baseStyles,
-      opacity: classification ? CLASSIFICATION_OPACITY : 1,
-    },
-  };
-};
 
 
 export const getHighlightedLegalMoves = (legalMoves: Move[]) => {
