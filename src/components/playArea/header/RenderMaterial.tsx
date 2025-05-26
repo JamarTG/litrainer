fe2973dbfbfce541c353d41214a0eb661a0ee74a
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-05-25 20:14:40 -0500

    Remove Unnecessary State and useEffect from RenderMaterial

--- src/components/playArea/header/RenderMaterial.tsx
+++ src/components/playArea/header/RenderMaterial.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from "react";
+import { FC } from "react";
 import { Materials } from "../../../types/eval";
 import { determineColorLeadingInMaterial } from "../../../utils/chess/material";
 import PieceIcon from "./PieceIcons";
@@ -10,12 +10,7 @@ interface RenderMaterialProps {
 }
 
 const RenderMaterial: FC<RenderMaterialProps> = ({ material, color }) => {
-  useEffect(() => {
-    setMaterialCount(determineColorLeadingInMaterial(material, color));
-  }, [color, material]);
-
-  const [materialCount, setMaterialCount] = useState<number>(0);
-
+  const materialCount = determineColorLeadingInMaterial(material, color);
   return (
     <div className="flex justify-center items-center ">
       {Object.entries(color === "b" ? material.b : material.w).map(([piece, count]) =>
@@ -27,7 +22,7 @@ const RenderMaterial: FC<RenderMaterialProps> = ({ material, color }) => {
             {[...Array(count)].map(() => (
               <PieceIcon
                 piece="bishop"
-                size={24}
+                size={18}
               />
             ))}
           </div>
