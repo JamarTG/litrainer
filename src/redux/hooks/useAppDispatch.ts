77ade134c02d4affeddbe2bb12f5b6496df83d14
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-05-26 13:51:14 -0500

    change redux file and folder names

--- src/redux/hooks/useAppDispatch.ts
+++ src/redux/hooks/useAppDispatch.ts
@@ -1,6 +1,4 @@
 import { useDispatch } from 'react-redux';
 import { AppDispatch } from '../store';
-// export type { GameState } from '../slices/gameSlices';
-// export type { PuzzleState } from '../slices/puzzleSlices';
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
