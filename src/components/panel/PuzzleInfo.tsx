b966230fc54388ceb55d4669476b3e7aebaf00ec
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-05-28 13:19:52 -0500

    Reduced gap and justify-start for smaller screens

--- src/components/panel/PuzzleInfo.tsx
+++ src/components/panel/PuzzleInfo.tsx
@@ -17,38 +17,40 @@ const PuzzleInfo = () => {
   const imageAlt = classification ?? puzzle.evaluation.judgment?.name ?? "move quality";
 
   return (
-    <div className="flex flex-col h-96 justify-start items-center sm:justify-center p-2 gap-4">
-      {playedMove && classification ? (
-        <div className="flex flex-col items-center justify-center gap-2">
-          <div className="flex items-center gap-2">
-            <img
-              src={imageSrc}
-              width={40}
-              alt={imageAlt}
-            />
-            <div className="flex flex-col items-start justify-center text-sm text-gray-800 dark:text-white">
-              <small className="text-sm">
-                <span className="text-xl font-bold">{playedMove}</span>
-              </small>
-              <p className="text-gray-600 dark:text-gray-300 text-lg">{bestMove}</p>
-            </div>
-          </div>
-        </div>
-      ) : (
-        <div className="flex flex-col items-center gap-5">
-          <div className="flex flex-col items-center gap-2">
-            <div className="flex gap-2 items-center">
+    <div className="flex flex-col h-96 bg-red-600 justify-start items-center md:justify-center p-2 gap-2">
+      <div className="bg-red-600">
+        {playedMove && classification ? (
+          <div className="flex flex-col items-center justify-center gap-2">
+            <div className="flex items-center gap-2">
+              <img
+                src={imageSrc}
+                width={40}
+                alt={imageAlt}
+              />
               <div className="flex flex-col items-start justify-center text-sm text-gray-800 dark:text-white">
-                <span className="text-xl font-bold">
-                  {" "}
-                  You played <span className="text-2xl font-bold">{puzzle.userMove?.san}</span>
-                </span>
-                <p className="text-gray-600 dark:text-gray-300 text-lg">Try to find a better move</p>
+                <small className="text-sm">
+                  <span className="text-xl font-bold">{playedMove}</span>
+                </small>
+                <p className="text-gray-600 dark:text-gray-300 text-lg">{bestMove}</p>
               </div>
             </div>
           </div>
-        </div>
-      )}
+        ) : (
+          <div className="flex flex-col items-center gap-5">
+            <div className="flex flex-col items-center gap-2">
+              <div className="flex gap-2 items-center">
+                <div className="flex flex-col items-start justify-center text-sm text-gray-800 dark:text-white">
+                  <span className="text-xl font-bold">
+                    {" "}
+                    You played <span className="text-2xl font-bold">{puzzle.userMove?.san}</span>
+                  </span>
+                  <p className="text-gray-600 dark:text-gray-300 text-lg">Try to find a better move</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
 
       <Navigation />
     </div>
