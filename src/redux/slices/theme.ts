971ba10360c4407f26cd7a5ab544d9aba6a51ff2
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-05-28 10:03:41 -0500

    Persist user preferred theme

--- src/redux/slices/theme.ts
+++ src/redux/slices/theme.ts
@@ -1,27 +1,28 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-
+import { loadFromLocalStorage, saveToLocalStorage } from "../../utils/storage";
 
 export type Theme = "light" | "dark";
 
 export interface ThemeState {
-    theme: Theme    
+  theme: Theme;
 }
 
 const initialState: ThemeState = {
-    theme: "light", 
+  theme: loadFromLocalStorage<Theme>("theme", "light"),
 };
 
 const themeSlice = createSlice({
-    name: "theme",
-    initialState,
-    reducers: {
-        toggleTheme(state) {
-            state.theme = state.theme === "light" ? "dark" : "light";
-        },
-        setTheme(state, action : PayloadAction<ThemeState>) {
-            state.theme = action.payload.theme;
-        }
+  name: "theme",
+  initialState,
+  reducers: {
+    toggleTheme(state) {
+      state.theme = state.theme === "light" ? "dark" : "light";
+      saveToLocalStorage("theme", state.theme);
     },
+    setTheme(state, action: PayloadAction<ThemeState>) {
+      state.theme = action.payload.theme;
+    },
+  },
 });
 
 export const { toggleTheme, setTheme } = themeSlice.actions;
