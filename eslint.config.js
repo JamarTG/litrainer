d6cf063eebe3d84cad448341ee4268c09cf38223
max <maxhumpherys@gmail.com>
2025-06-03 19:01:00 -0600

    react-hooks and react-refresh plugins to mimic prior config

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,31 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
 import globals from "globals";
 
-export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended, {
-  name: "litrainer lint",
-  languageOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    parser: tseslint.parser,
-    globals: {
-      ...globals.browser,
-      ...globals.es2020
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  reactHooks.configs["recommended-latest"],
+  reactRefresh.configs.recommended,
+  {
+    name: "litrainer lint",
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parser: tseslint.parser,
+      globals: {
+        ...globals.browser,
+        ...globals.es2020
+      },
+      parserOptions: {
+        project: false
+      }
     },
-    parserOptions: {
-      project: false
+    plugins: { "@typescript-eslint": tseslint.plugin },
+    rules: {
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }]
     }
-  },
-  plugins: { "@typescript-eslint": tseslint.plugin },
-  rules: {}
-});
+  }
+);
