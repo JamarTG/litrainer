7aeeac73ee52ea464cf7ba8fd33651a62fcdad2b
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-06-05 20:48:20 -0500

    improve getGameStatusDescription

--- src/utils/status.ts
+++ src/utils/status.ts
@@ -1,19 +1,18 @@
 import { LichessPlayer } from "@/types/lichess";
 
-const gameStatusMap: Record<string, (player: string) => string> = {
-  mate: (player) => (player ? `${player} won by checkmate.` : "Draw."),
-  resign: (player) => (player ? `${player} won by resignation.` : "Draw."),
-  stalemate: () => "The game ended in a stalemate.",
-  timeout: (player) => (player ? `${player} won by timeout.` : "Draw."),
-  draw: () => "The game ended in a draw.",
-  outoftime: (player) => (player ? `${player} won by timeout.` : "Draw."),
-  cheat: (player) => (player ? `${player} won by cheating.` : "Draw."),
-  noStart: (player) => (player ? `${player} failed to start the game.` : "Draw."),
-  unknownFinish: () => "The game ended unexpectedly."
+const getGameStatusDescription = (status: string, player: LichessPlayer): string => {
+  const gameStatusMap: Record<string, (player: string) => string> = {
+    mate: (player) => (player ? `${player} won by checkmate.` : "Draw."),
+    resign: (player) => (player ? `${player} won by resignation.` : "Draw."),
+    stalemate: () => "The game ended in a stalemate.",
+    timeout: (player) => (player ? `${player} won by timeout.` : "Draw."),
+    draw: () => "The game ended in a draw.",
+    outoftime: (player) => (player ? `${player} won by timeout.` : "Draw."),
+    cheat: (player) => (player ? `${player} won by cheating.` : "Draw."),
+    noStart: (player) => (player ? `${player} failed to start the game.` : "Draw."),
+    unknownFinish: () => "The game ended unexpectedly."
+  };
+  return gameStatusMap[status]?.(player.user.name) || `Unknown game status: ${status}.`;
 };
 
-function getGameStatusDescription(status: string, player: LichessPlayer): string {
-  return gameStatusMap[status]?.(player.user.name) || `Unknown game status: ${status}.`;
-}
-
-export { gameStatusMap, getGameStatusDescription };
+export { getGameStatusDescription };
