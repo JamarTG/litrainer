57e59adbe6680f63672838f66c3c269e07b93d24
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2026-02-08 13:34:33 -0500

    add github icon as link

--- src/components/layout/NavbarLayout.tsx
+++ src/components/layout/NavbarLayout.tsx
@@ -1,13 +1,17 @@
 import { PropsWithChildren } from "react";
 import ThemeChanger from "../common/ThemeChanger";
-import GithubButton from "../shared/GithubLink";
+import github from "/icons/github.svg";
+import { ICON_SIZES } from "@/constants/icons";
 
 const NavbarLayout: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <section className="relative min-h-screen dark:bg-zinc-900  ">
       <nav className="hidden sm:flex p-2 pb-6 justify-between items-center z-20 relative">
         <ThemeChanger />
-        <GithubButton />
+        <a target="_blank" href="https://github.com/JamarTG/litrainer">
+          <img src={github}  alt="github image link" width={ICON_SIZES.LARGE}/>
+        </a>
+
       </nav>
       <main className="relative z-10">{children}</main>
     </section>
