8c8109ea1a4f257b23ab90a1bef945d55257f34c
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2026-02-16 14:40:39 -0500

    Switch to @mdi/react Material Design Icons for time controls

--- src/components/shared/GameSpeedIcon.tsx
+++ src/components/shared/GameSpeedIcon.tsx
@@ -1,17 +1,33 @@
+
+import Icon from "@mdi/react";
+import {
+  mdiRabbit,
+  mdiFire,
+  mdiBullet,
+  mdiSend,
+  mdiTurtle
+} from "@mdi/js";
 import { GameType } from "@/typing/types";
-import { TimeControlIcons } from "@/constants/icons";
 import { FC } from "react";
 
 interface GameSpeedIconProps {
   speed: GameType;
-  size: number;
+  size?: number;
 }
 
-const GameSpeedIcon: FC<GameSpeedIconProps> = ({ speed, size }) => {
+
+const TimeControlIcons: Record<GameType, string> = {
+  rapid: mdiRabbit,
+  blitz: mdiFire,
+  bullet: mdiBullet,
+  correspondence: mdiSend,
+  classical: mdiTurtle
+};
+
+const GameSpeedIcon: FC<GameSpeedIconProps> = ({ speed, size = 22 }) => {
+  const path = TimeControlIcons[speed];
   return (
-    <span className="icon" style={{ fontSize: size }}>
-      {TimeControlIcons[speed]}
-    </span>
+    <Icon path={path} size={size / 24} style={{ display: "inline-flex", verticalAlign: "middle" }} />
   );
 };
 
