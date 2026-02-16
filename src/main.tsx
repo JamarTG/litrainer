0de27c5e8d0fd6b6c5e7105bb949c8538e428c62
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2026-02-16 14:45:53 -0500

    Remove forced dark mode; now only default

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,6 @@ import App from "./App";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
-// Force dark mode by default
-document.documentElement.classList.add("dark");
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
