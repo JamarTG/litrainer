b022f12402b35456a64e26162e4c208f9c8bdc13
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2026-02-16 12:19:06 -0500

    fix: correct Lichess profile URL to remove extra slash

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,6 +1,6 @@
 export const LICHESS_URLS = {
   Base: "https://lichess.org/",
-  Profile: "https://lichess.org/@/",
+  Profile: "https://lichess.org/@",
   GamesAPI: "https://lichess.org/api/games/user/",
   Openings: "https://lichess.org/opening/",
   AutoCompleteUser: "https://lichess.org/api/player/autocomplete"
