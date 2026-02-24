b3dfe19b1827f55bcfbecf325adc4551ea73c378
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2026-02-24 16:17:00 -0500

    Feat: Hide panel content when modal is showing for a clear UI

--- src/features/panel/ui/Panel.tsx
+++ src/features/panel/ui/Panel.tsx
@@ -1,11 +1,16 @@
 import PanelBody from "./PanelBody";
 import PanelHeader from "./PanelHeader";
 
-const Panel = () => {
+
+interface PanelProps {
+  hideContent?: boolean;
+}
+
+const Panel = ({ hideContent = false }: PanelProps) => {
   return (
     <div className="h-full w-full md:max-w-96 sm:h-full sm:mb-2 md:min-h-[550px] md:bg-[var(--color-surface-strong)] flex flex-col gap-1 md:gap-4 shadow-sm border border-[var(--color-border)] rounded-md">
-      <PanelHeader />
-      <PanelBody />
+      {!hideContent && <PanelHeader />}
+      {!hideContent && <PanelBody />}
     </div>
   );
 };
